4469d84470c20970470c2d29f61d870bc72d3eab
Mephit299 <levihogdal@gmail.com>
2023-08-31 11:01:20 +0200

    ✨ variabel kod

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Hello world");
+console.log("Hello world")
+
+let name; // definerar
+firstname = "bob" // tilderar ett värde
+
+let lastname = "bobson" // definerar och tilldelar ett värde
+
+let num = 23
+
+console.log(typeof(num))
+
+const NAME ="Dave"
+console.log(NAME)
+let i
+
+for(let i = 0; i<10; i++){
+    const NAME = "Jim"
+    console.log(NAME)
+}
+console.log(i)
+
+var skola = "NTI"
+console.log(skola)
+
+let a = "Jim" // string
+let b = `Jim` // string rekomenderas
+let c = `Mitt namn är ${a}`
+let d = `Mitt namn är ` + b
+
+let e = 12 // num 
+let f = 16.7 /// num
+
+let g = true // bolean
+let h = false // bolean
+
+console.log(c, e, g)
